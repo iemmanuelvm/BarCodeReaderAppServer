2e4ec9936dfdcb46fa6461894be9b8a28c53fbea
iemmanuelvm <iemmanuelvm@gmail.com>
2024-06-26 17:55:26 -0600

    interface with react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
+import asyncio
 
 app = FastAPI()
 
@@ -11,16 +12,22 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+connected_clients = []
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("Client connected")
+    connected_clients.append(websocket)
     try:
         while True:
             data = await websocket.receive_text()
             print(f"Message received: {data}")
+            for client in connected_clients:
+                await client.send_text(data)
     except WebSocketDisconnect:
         print("Client disconnected")
+        connected_clients.remove(websocket)
     except Exception as e:
         print(f"Connection error: {e}")
     finally:
